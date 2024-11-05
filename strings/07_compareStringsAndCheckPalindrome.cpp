90293db3c1b607f6921142f8d21ae2c5c8f829f3
Rahul Kumar <therahulkumar135@gmail.com>
2024-11-05 09:00:45 +0530

    Learn and implement basic string comparison logic in C++

---
+++ strings/07_compareStringsAndCheckPalindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Comparing strings
+    char A[] = "Painter";
+    char B[] = "Painting";
+    int i, j;
+
+    for(i = 0, j = 0; A[i] != '\0' && B[j] != '\0'; i++, j++) {
+        if(A[i] != B[j]) {
+            break;
+        }
+    }
+
+    if(A[i] == B[j]) 
+        cout << "Equal" << endl;
+    else if(A[i] < B[j]) 
+        cout << "Smaller" << endl;
+    else 
+        cout << "Greater" << endl;
+    
+    return 0;
+}
