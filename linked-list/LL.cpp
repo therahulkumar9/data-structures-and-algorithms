abf5f98c0d915506a81a4877cf85b84fc8394489
Rahul Kumar <therahulkumar135@gmail.com>
2024-12-24 02:46:13 +0530

    Added function to print the linked list

--- linked-list/LL.cpp
+++ linked-list/LL.cpp
@@ -17,9 +17,41 @@ class Node{
 
     }
 };
+
+
+Node* convertArr2LL(vector<int> &arr) {
+    if (arr.empty()) return nullptr; 
+
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+
+// Function to print the linked list
+void printList(Node* head){
+    Node* current = head;
+    while(current != nullptr){
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
 int main(){
     vector<int> arr={2,5,8,7};
     Node* y= new Node(arr[0]);
     cout<<y<<'\n'; // returns the memory value
     cout<<y->data<<'\n'; // returns the data stored at that memory point
+    Node* head = convertArr2LL(arr);
+    cout << "Linked List: ";
+    cout << head->data << "\n";
+    printList(head);
+
+    return 0;
 }
